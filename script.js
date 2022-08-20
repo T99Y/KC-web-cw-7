aef3435b35e478f05eb628d30ca4808b0ff01343
T99Y <108480373+T99Y@users.noreply.github.com>
2022-08-20 09:25:40 +0300

    done

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+function BMI(weight, height){
+let result = weight / (height * height);
+return result; 
+}
+console.log(BMI(70,1.70));
+function status(BMI) {
+if (BMI<18.5) {
+    return("لديك نقص في الوزن");
+} else if(BMI>=18.5 &&BMI<25){
+    return "وزنك صحي";
+}else{
+    return "لديك زيادة في الوزن";
+}
+}
 
+function calculate(){
+let weight = document.getElementById("weight").value;
+let height = document.getElementById("height").value;
+ 
+let BMI = BMI(weight, height);
+
+let desc = status (bmi);
+let div = document.getElementById("result");
+
+div.innerText = "bmi + " == " + desc";
+
+
+}
